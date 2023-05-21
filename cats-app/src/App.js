db08b77edbb86e073006c8f17827bd017090b6ae
walter <arenalwm@gmail.com>
2023-05-21 00:59:30 -0300

    traslado app

---
+++ cats-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CatsView from "./cats/views/CatsView";
+
+function App() {
+  return (
+    <div className="App">
+      <CatsView/>
+    </div>
+  );
+}
+
+export default App;
---
+++ cats-app/src/cats/hooks/useFetch.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [refresh, setRefresh] = useState(false);
+  const refetch = () => setRefresh(!refresh);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error("Algo salió mal");
+        }
+
+        const data = await response.json();
+        setData(data);
+      } catch (err) {
+        setError("Algo salió mal");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [refresh]);
+
+  return {
+    data,
+    isLoading,
+    error,
+    refetch,
+  };
+};
+
+export default useFetch;
